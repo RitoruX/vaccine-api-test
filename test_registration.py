ee419472aad884c9e8a922230229f888e7237950
RitoruX <ppittayut.ritoru@gmail.com>
2021-11-18 21:43:54 +0700

    change testing sample to related with API parameters.

--- test_registration.py
+++ test_registration.py
@@ -6,7 +6,7 @@ API_URL = "https://wcg-apis.herokuapp.com/registration"
 class TestRegistrationApi(unittest.TestCase):
     """Test for registration endpoint of WCG API.."""
 
-    def sample_body(self, address = "Bangkok", birth_date = "2000-02-02", citizen_id = "1234567666666", name = "SuphanburiPrajantakham", occupation = "student", surname = "Wang"):
+    def sample_body(self, address = "Bangkok", birth_date = "2000-02-02", citizen_id = "1234567666666", name = "SuphanburiPrajantakham", occupation = "student", surname = "Wang", is_risk = False, phone_number = "0951903000"):
         """Method create user's info for different input testing."""
         body = {
         "address": f"{address}",
@@ -14,27 +14,24 @@ class TestRegistrationApi(unittest.TestCase):
         "citizen_id": f"{citizen_id}",
         "name": f"{name}",
         "occupation": f"{occupation}",
-        "surname": f"{surname}"
+        "surname": f"{surname}",
+        "phone_number": f"{phone_number}",
+        "is_risk": is_risk
         }
         return body
 
     def setUp(self):
         """Initialize the response feedback and clear last testing object."""
-        requests.delete("https://wcg-apis.herokuapp.com/citizen", data = self.sample_body())
+        requests.delete("https://wcg-apis.herokuapp.com/registration/1234567666666")
         self.INVALID_ID = {'feedback': 'registration failed: invalid citizen ID'}
-        self.SUCCESS = {"feedback": "registration success!"}
+        self.SUCCESS = "registration success!"
         self.MISSING_ATTRIBUTE = {'feedback': 'registration failed: missing some attribute'}
-
-    def test_request_response(self):
-        """Send GET request to API for check avaliability from the status code."""
-        self.response = requests.get(API_URL)
-        self.assertEqual(self.response.status_code, 200)
 
     def test_post_registration(self):
         """Send POST request to API for test comlete user's info registration."""
         self.response = requests.post(API_URL, self.sample_body())
-        self.assertEqual(self.response.status_code, 200)
-        self.assertEqual(self.response.json(), self.SUCCESS)
+        self.assertEqual(self.response.status_code, 201)
+        self.assertEqual(self.response.json()['feedback'], self.SUCCESS)
 
     def test_post_registration_with_letter_id(self):
         """Send POST request to API with replaced user's ID with letter."""
@@ -87,19 +84,25 @@ class TestRegistrationApi(unittest.TestCase):
     def test_post_registration_with_numeric_name(self):
         """Test registration with a numeric name. (They should be fail to registration.)"""
         self.response = requests.post(API_URL, self.sample_body(name = 1101))
-        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, 201)
         self.assertNotEqual(self.response.json(), self.SUCCESS)
 
     def test_post_registration_with_numeric_surname(self):
         """Test registration with a numeric surname. (They should be fail to registration.)"""
         self.response = requests.post(API_URL, self.sample_body(surname = 1101))
-        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, 201)
         self.assertNotEqual(self.response.json(), self.SUCCESS)
 
     def test_post_registration_with_numeric_occupation(self):
         """Test registration with a numeric occupation. (They should be fail to registration.)"""
         self.response = requests.post(API_URL, self.sample_body(occupation = 1101))
-        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, 201)
+        self.assertNotEqual(self.response.json(), self.SUCCESS)
+
+    def test_post_registration_with_not_boolean_is_rick(self):
+        """Test registration with a not boolean is_risk parameter. (They should be fail to registration.)"""
+        self.response = requests.post(API_URL, self.sample_body(is_risk = "True"))
+        self.assertEqual(self.response.status_code, 201)
         self.assertNotEqual(self.response.json(), self.SUCCESS)
 
 if __name__ == '__main__':
